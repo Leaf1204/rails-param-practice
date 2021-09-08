d0ca609a7cd360b0a571b103f0e9af5998397c71
Leanne A Frisinger <67919464+Leaf1204@users.noreply.github.com>
2021-09-07 18:56:07 -0600

    cat search working

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,7 @@ class SearchesController < ApplicationController
 
     def new
         @search = Search.new
-        @categories = Book.uniq.pluck(:category)
+        @categories = Book.distinct.pluck(:category)
     end 
 
     def create
